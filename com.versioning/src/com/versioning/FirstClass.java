3ca4f67cee500c322b9a782ae0192d098497d278
Daniel <hanadavid93@gmail.com>
2019-03-18 10:17:09 +0100

    new changes 2

--- com.versioning/src/com/versioning/FirstClass.java
+++ com.versioning/src/com/versioning/FirstClass.java
@@ -3,8 +3,41 @@ package com.versioning;
 public class FirstClass {
 
 	private int x;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getS() {
+		return s;
+	}
+
+	public void setS(String s) {
+		this.s = s;
+	}
+
+	public boolean isCheck() {
+		return check;
+	}
+
+	public void setCheck(boolean check) {
+		this.check = check;
+	}
+
 	private int y;
 	private String s;
 	private boolean check;
-	
+
 }
